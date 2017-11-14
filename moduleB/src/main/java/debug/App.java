b90d433637b2ab061ca225e767deaa9e4e31c4e1
unknown <g8931@yajuenet.internal>
2017-11-14 20:32:15 +0800

    模块化引用

---
+++ moduleB/src/main/java/debug/App.java
@@ -0,0 +1,15 @@
+package debug;
+
+import android.app.Application;
+
+/**
+ * Created by g8931 on 2017/11/14.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ moduleB/src/main/java/yummylau/moduleb/MainActivity.java
@@ -0,0 +1,18 @@
+package yummylau.moduleb;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by g8931 on 2017/11/14.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abc_search_view);
+    }
+}
