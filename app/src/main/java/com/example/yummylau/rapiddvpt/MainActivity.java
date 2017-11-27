beb32dca05a8db5c6ed2bb72a857373d0da05d08
yummyLau <yummyl.lau@gmail.com>
2017-11-27 21:34:47 +0800

    新增抽屉实现

--- app/src/main/java/com/example/yummylau/rapiddvpt/MainActivity.java
+++ app/src/main/java/com/example/yummylau/rapiddvpt/MainActivity.java
@@ -4,7 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 
